df36a928fa4d47112e43c28a4b8e38db46418d9b
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-08-12 15:21:15 -0500

    commit tree 73a9daf236e534f7a2be306f19986482d51d3f93

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,6 +6,16 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    return movieData.map( (movie, id) => 
+     <MovieCard
+        key={id}
+        title={movie.title}
+        IMDBRating={movie.IMDBRating}
+        genres={movie.genres}
+        poster={movie.poster}
+  
+     />
+    )
   }
 
   render() {
